221b929ad03242116f9bd1d5eefa8e784c3b589a
Aaron K <aaron@pebbleroad.com>
2018-04-03 10:20:48 +0800

    Fixed proptype bool for keepTooltip

--- src/createSliderWithTooltip.jsx
+++ src/createSliderWithTooltip.jsx
@@ -10,7 +10,7 @@ export default function createSliderWithTooltip(Component) {
       tipFormatter: PropTypes.func,
       handleStyle: PropTypes.oneOfType([PropTypes.object, PropTypes.arrayOf(PropTypes.object)]),
       tipProps: PropTypes.object,
-      keepTooltip: PropTypes.boolean,
+      keepTooltip: PropTypes.bool,
     };
     static defaultProps = {
       tipFormatter(value) { return value; },
